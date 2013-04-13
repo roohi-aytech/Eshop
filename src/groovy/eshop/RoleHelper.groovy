d60118de1b6e8eb6c63be433bac680bdd8df4218
roohi <srmo642002@gmail.com>
2013-04-13 15:34:35 +0430

    angularjs null values in select

--- src/groovy/eshop/RoleHelper.groovy
+++ src/groovy/eshop/RoleHelper.groovy
@@ -14,5 +14,9 @@ class RoleHelper {
     public static final String ROLE_PRODUCT_TYPE_ADMIN = "ROLE_PRODUCT_TYPE_ADMIN";
     public static final String ROLE_PRICE_ADMIN = "ROLE_PRICE_ADMIN";
     public static final String ROLE_CUSTOMER = "ROLE_CUSTOMER";
+    public static final String ROLE_PRODUCT_ADD = "ROLE_PRODUCT_ADD";
+    public static final String ROLE_PRODUCT_ADD_EDIT = "ROLE_PRODUCT_ADD_EDIT";
+    public static final String ROLE_PRODUCER_ADD = "ROLE_PRODUCER_ADD";
+    public static final String ROLE_PRODUCER_ADD_EDIT = "ROLE_PRODUCER_ADD_EDIT";
 
 }
