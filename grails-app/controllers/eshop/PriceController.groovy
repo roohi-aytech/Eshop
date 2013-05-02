77d2376c8b6e7cfb4f262a16a397cd90d96560c1
zohreh-star <zohreh.alavi@gmail.com>
2013-05-02 18:58:03 +0430

    new version

--- grails-app/controllers/eshop/PriceController.groovy
+++ grails-app/controllers/eshop/PriceController.groovy
@@ -36,8 +36,6 @@ class PriceController {
         if (params.id) {
             priceInstance = Price.get(params.id)
             priceInstance.properties = params
-        } else {
-            priceInstance = new Price(params)
             priceInstance.variationValues = []
             def variations = Variation.findAllByBaseProduct(Product.get(params.product.id))
 
@@ -45,47 +43,82 @@ class PriceController {
                 priceInstance.variationValues.add(VariationValue.get(params."variation_${it.id}"))
             }
         }
+        else {
+            priceInstance = new Price(params)
+            priceInstance.variationValues = []
+            def variations = Variation.findAllByBaseProduct(Product.get(params.product.id))
 
-        def lastPrice = Price.createCriteria().list {
-                isNull('endDate')
-                eq('product', priceInstance.product)
-                variationValues {
-                        'in'('id', priceInstance.variationValues.collect{it.id})
-//                    priceInstance.variationValues.each(){
-//                        and{
-//                        eq("variationGroup",it?.variationGroup)
-//                        eq("id", it?.id)
-//                        }
-//                    }
+            variations.each {
+                priceInstance.variationValues.add(VariationValue.get(params."variation_${it.id}"))
+            }
+            boolean tmp
+            def lastPrice
+            def allLastPrices = Price.findByProductAndEndDateIsNull(priceInstance.product)
+            allLastPrices.each {
+                tmp = true
+                it.variationValues.each {
+                    if(!priceInstance.variationValues.collect{it.id}.contains(it.id)){
+                        tmp = false
+                    }
+                }
+                if (tmp == true){
+                    lastPrice = it
                 }
             }
-
             if (lastPrice) {
-                lastPrice[0].endDate = new Date()
-                lastPrice[0].save()
+                lastPrice.endDate = new Date()
+                lastPrice.save()
             }
-
-
+        }
         priceInstance.startDate = new Date()
         priceInstance.rialPrice = priceInstance.currency ? priceInstance.price * priceInstance.currency.exchangeRate : priceInstance.price
 
-        def defaultPrice = Price.createCriteria().list {
-            eq('product', priceInstance.product)
-            eq('defaultPrice', true)
-            variationValues {
-                'in'('id', priceInstance.variationValues.collect{it.id})
+        def tmp
+        def defaultPrice
+        def allPrices = Price.findAllByProductAndDefaultPrice(priceInstance.product, true)
+
+        allPrices.each {
+            tmp = true
+            it.variationValues.each {
+                if(!priceInstance.variationValues.collect{it.id}.contains(it.id)){
+                    tmp = false
+                }
+            }
+            if (tmp == true){
+                defaultPrice = it
             }
         }
+
 
         if (priceInstance.getDefaultPrice() == true) {
-            if (defaultPrice.size() > 0) {
-                defaultPrice[0].defaultPrice = false
-                defaultPrice[0].save()
+            if (defaultPrice) {
+                defaultPrice.defaultPrice = false
+                defaultPrice.save()
             }
         }
 
-        if(defaultPrice.size() == 0)
+        if(defaultPrice == null)
             priceInstance.defaultPrice = true
+
+        if(defaultPrice == null & !priceInstance.getDefaultPrice()){
+            def price
+            def Prices = Price.findAllByProduct(priceInstance.product)
+            for (it in Prices) {
+                tmp = true
+                it.variationValues.each {
+                    if (!priceInstance.variationValues.collect { it.id }.contains(it.id)) {
+                        tmp = false
+                    }
+                }
+                if (tmp == true) {
+                    it.defaultPrice = true
+                    it.save()
+                    break
+                }
+            }
+        }
+
+
 
         if (priceInstance.validate() && priceInstance.save()) {
             render priceInstance as JSON
@@ -93,19 +126,28 @@ class PriceController {
             render(template: "form", model: [priceInstance: priceInstance])
     }
 
+    def setDefaultValueForTheFirstPrice(){
+
+    }
 
     def delete() {
         def priceInstance = Price.get(params.id)
         if (priceInstance.defaultPrice == true) {
-            def prices = Price.createCriteria().list {
-                eq('product', priceInstance.product)
-                variationValues {
-                    'in'('id', priceInstance.variationValues.collect{it.id})
+            def tmp
+            def price
+            def allPrices = Price.findAllByProductAndDefaultPrice(priceInstance.product, false)
+            for (it in allPrices) {
+                tmp = true
+                it.variationValues.each {
+                    if (!priceInstance.variationValues.collect { it.id }.contains(it.id)) {
+                        tmp = false
+                    }
                 }
-            }
-            if (prices) {
-                prices[0].defaultPrice = true
-                prices[0].save()
+                if (tmp == true) {
+                    it.defaultPrice = true
+                    it.save()
+                    break
+                }
             }
         }
 
