bddc5ecccd0aa9b0cb4d728b01700c17f80e72e1
roohi <srmo642002>
2013-06-26 11:35:07 +0430

    terms and conditions

--- grails-app/taglib/eshop/EshopTagLib.groovy
+++ grails-app/taglib/eshop/EshopTagLib.groovy
@@ -12,7 +12,7 @@ class EshopTagLib {
         request.setAttribute("product", product)
         HashSet definedProductTypes = new HashSet()
         request.setAttribute("definedProductTypes", definedProductTypes)
-        product.productTypes?.sort { it.name }?.each {
+        product.productTypes?.findAll {!it.deleted}?.sort { it.name }?.each {
             request.setAttribute("productType", it)
             out << renderProductTypeAttributes()
             request.removeAttribute("productType")
@@ -31,7 +31,7 @@ class EshopTagLib {
         out << "<fieldset class='form'>"
         out << "<legend>${productType.name}</legend>"
 
-        def attributeTypes = AttributeType.findAllByProductTypeAndCategoryIsNull(productType)
+        def attributeTypes = AttributeType.findAllByProductTypeAndCategoryIsNullAndDeleted(productType,false)
         attributeTypes?.sort { it.sortIndex }?.each {
             request.setAttribute("attribute", it)
             out << renderAttribute()
@@ -61,14 +61,14 @@ class EshopTagLib {
         out << "<fieldset class='form'>"
         out << "<legend>${attributeCategory?.name}</legend>"
 
-        def attributeTypes = AttributeType.findAllByCategory(attributeCategory)
+        def attributeTypes = AttributeType.findAllByCategoryAndDeleted(attributeCategory,false)
         attributeTypes?.sort { it.sortIndex }?.each {
             request.setAttribute("attribute", it)
             out << renderAttribute()
             request.removeAttribute("attribute")
         }
 
-        def attributeCategories = AttributeCategory.findAllByParentCategory(attributeCategory)
+        def attributeCategories = AttributeCategory.findAllByParentCategoryAndDeleted(attributeCategory,false)
         attributeCategories.sort { it.name }.each {
             request.setAttribute("attributeCategory", it)
             out << renderAttributeCategory()
