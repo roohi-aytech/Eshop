dc3b134dc1f381e23d2e14ac399278aae8ac8f34
FarzinZaker <f_zaker@yahoo.com>
2013-07-09 13:33:02 +0430

    bug fix

--- grails-app/controllers/eshop/SiteController.groovy
+++ grails-app/controllers/eshop/SiteController.groovy
@@ -375,8 +375,11 @@ class SiteController {
         mongoService.storeProduct(product)
 
         //fill zoomable property of images
-        imageService.getImageSize(product.mainImage, product)
-        product.mainImage.dynamicProperties.zoomable = product.mainImage.dynamicProperties.width >= 700 || product.mainImage.dynamicProperties.height >= 700
+        if (product.mainImage) {
+            imageService.getImageSize(product.mainImage, product)
+            product.mainImage.dynamicProperties.zoomable = product.mainImage.dynamicProperties.width >= 700 || product.mainImage.dynamicProperties.height >= 700
+        }
+
         product?.images?.findAll { it?.id != product?.mainImage?.id }?.each {
             imageService.getImageSize(it, product)
             it.dynamicProperties.zoomable = it.dynamicProperties.width >= 700 || it.dynamicProperties.height >= 700
