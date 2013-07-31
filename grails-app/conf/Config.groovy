452ec7de1b84c802c3deaf7a77d1f27bdb56817c
farzin <farzin.zaker@gmail.com>
2013-07-31 17:12:04 +0430

    uncommitable

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -20,24 +20,24 @@ grails.plugin.invitation.facebook.key = 'your APP ID'
 grails.plugin.invitation.facebook.secret = 'your App Secret'
 
 grails.plugin.invitation.google.enabled = true
-grails.plugin.invitation.google.key = 'local.zanbil.ir'
-grails.plugin.invitation.google.secret = 'FrsEytRsLc0UXI2Gn9t3qnL9'
+grails.plugin.invitation.google.key = 'www.zanbil.ir'
+grails.plugin.invitation.google.secret = 'ovX8LLv2vv9_3ZzfX-iXrF0s'
 
 grails.plugin.invitation.yahoo.enabled = true
-grails.plugin.invitation.yahoo.key = 'dj0yJmk9YXJxSHV6Z1ZKdHBQJmQ9WVdrOVZGSkVTR04zTlRJbWNHbzlNVGc0T0RRMU9ERTJNZy0tJnM9Y29uc3VtZXJzZWNyZXQmeD0zMA--'
-grails.plugin.invitation.yahoo.secret = 'def0728b7613c04f0f8dbd7b70b9f03366222b5b'
+grails.plugin.invitation.yahoo.key = 'dj0yJmk9WGZtVlJhS0xVem8wJmQ9WVdrOWFHUnZlbEYzTm5FbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD1kYw--'
+grails.plugin.invitation.yahoo.secret = '0df982690055e700631968fc65b8d01d67ee3f6b'
 
 grails.plugin.invitation.twitter.enabled = true
-grails.plugin.invitation.twitter.key = 'Your Consumer Key'
-grails.plugin.invitation.twitter.secret = 'Your Consumer Secret'
+grails.plugin.invitation.twitter.key = 'TvO3rfrjDcUvsOSqmJafg'
+grails.plugin.invitation.twitter.secret = 'GtPl4V1tKBnVLgy1ZnGERkcUnPIJExIpp2OCtZ98U'
 
 grails.plugin.invitation.linkedin.enabled = true
-grails.plugin.invitation.linkedin.key = '4hk58k17luv3'
-grails.plugin.invitation.linkedin.secret = 'PQugd9jS7HQPmyvy'
+grails.plugin.invitation.linkedin.key = 'djx324dkxfkz'
+grails.plugin.invitation.linkedin.secret = 'zJRKXHw2lGn3zcEC'
 
 grails.plugin.invitation.windowslive.enabled = true
-grails.plugin.invitation.windowslive.key = '000000004C0FA7EE'
-grails.plugin.invitation.windowslive.secret = '3Yig1lAtccllUf03Xx90jfT531EmOakB'
+grails.plugin.invitation.windowslive.key = '000000004C0FB2E9'
+grails.plugin.invitation.windowslive.secret = 'zGCQO8mV4G7JSZS16RJu3fxcDeoqCVlt'
 
 
 
