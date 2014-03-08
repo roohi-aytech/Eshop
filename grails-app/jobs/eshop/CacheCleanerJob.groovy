8c30ebaf8a8f0d1bf4bc3efa3d2e221e62f14f98
roohi <srmo642002>
2014-03-08 14:00:54 +0330

    Cache e price

--- grails-app/jobs/eshop/CacheCleanerJob.groovy
+++ grails-app/jobs/eshop/CacheCleanerJob.groovy
@@ -8,12 +8,38 @@ class CacheCleanerJob {
         simple repeatInterval: 120000 // execute job once in 2 minutes
     }
 
-    @CacheEvict(value = 'service', allEntries = true)
-    @CacheEvict(value = 'imageindex', allEntries = true)
-    @CacheEvict(value = 'feedatricles', allEntries = true)
-    @CacheEvict(value = 'feedposts', allEntries = true)
-    @CacheEvict(value = 'feednews', allEntries = true)
+
     def execute() {
+        clearFeed()
+        clearNews()
+        clearImage()
+        clearService()
+        clearPosts()
+    }
+
+    @CacheEvict(value = 'service', allEntries = true)
+    def clearService() {
         println 'All Service Cache Cleared: ' + new Date()
     }
+
+    @CacheEvict(value = 'imageindex', allEntries = true)
+    def clearImage() {
+        println 'All Image Cache Cleared: ' + new Date()
+    }
+
+    @CacheEvict(value = 'feedatricles', allEntries = true)
+    def clearFeed() {
+        println 'All Feed Cache Cleared: ' + new Date()
+    }
+
+    @CacheEvict(value = 'feedposts', allEntries = true)
+    def clearPosts() {
+        println 'All Post Cache Cleared: ' + new Date()
+    }
+
+    @CacheEvict(value = 'feednews', allEntries = true)
+    def clearNews() {
+        println 'All News Cache Cleared: ' + new Date()
+    }
+
 }
