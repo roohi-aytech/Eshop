22653a73e63d43ff3ca97cb906ffbe50e3ce0cee
roohi <srmo642002@gmail.com>
2014-07-24 09:19:38 +0430

    bigFooter

--- grails-app/jobs/eshop/NewsLetterJob.groovy
+++ grails-app/jobs/eshop/NewsLetterJob.groovy
@@ -13,56 +13,58 @@ class NewsLetterJob {
     }
 
     def execute(params) {
-        def newsLetterInstance = NewsLetterInstance.get((params.jobDataMap.newsLetterInstance as NewsLetterInstance).id)
-        if (newsLetterInstance.status == 'scheduled') {
-            newsLetterInstance.logs?.clear()
-            Customer.createCriteria().list {
-                or {
-                    newsLetterCategories {
-                        'in'('id', newsLetterInstance.newsLetter.categories.collect { it.id } ?: [0.toLong()])
+        def newsLetterInstance = NewsLetterInstance.get((params.jobDataMap?.newsLetterInstance as NewsLetterInstance)?.id)
+        if(newsLetterInstance) {
+            if (newsLetterInstance?.status == 'scheduled') {
+                newsLetterInstance.logs?.clear()
+                Customer.createCriteria().list {
+                    or {
+                        newsLetterCategories {
+                            'in'('id', newsLetterInstance.newsLetter.categories.collect { it.id } ?: [0.toLong()])
+                        }
+                        newsLetterProductTypes {
+                            'in'('id', newsLetterInstance.newsLetter.productTypes.collect { it.id } ?: [0.toLong()])
+                        }
                     }
-                    newsLetterProductTypes {
-                        'in'('id', newsLetterInstance.newsLetter.productTypes.collect { it.id } ?: [0.toLong()])
+                }.unique { it.id }.each { customer ->
+                    def log = new NewsLetterLog()
+                    log.status = 'scheduled'
+                    log.newsLetterInstance = newsLetterInstance
+                    log.customer = customer
+                    log.save()
+
+                    newsLetterInstance.addToLogs(log)
+                }
+                newsLetterInstance.status = 'started'
+                newsLetterInstance.save()
+            }
+
+            def newsLetterLog = NewsLetterLog.findByNewsLetterInstanceAndStatus(newsLetterInstance, 'scheduled')
+            if (newsLetterLog) {
+                newsLetterLog.sendDate = new Date()
+                try {
+                    mailService.sendMail {
+                        to newsLetterLog.customer.email
+                        subject newsLetterInstance.newsLetter.subject
+                        html newsLetterInstance.newsLetter.body
                     }
+                    newsLetterLog.status = 'sent'
                 }
-            }.unique {it.id}.each { customer ->
-                def log = new NewsLetterLog()
-                log.status = 'scheduled'
-                log.newsLetterInstance = newsLetterInstance
-                log.customer = customer
-                log.save()
-
-                newsLetterInstance.addToLogs(log)
-            }
-            newsLetterInstance.status = 'started'
-            newsLetterInstance.save()
-        }
-
-        def newsLetterLog = NewsLetterLog.findByNewsLetterInstanceAndStatus(newsLetterInstance, 'scheduled')
-        if (newsLetterLog) {
-            newsLetterLog.sendDate = new Date()
-            try {
-                mailService.sendMail {
-                    to newsLetterLog.customer.email
-                    subject newsLetterInstance.newsLetter.subject
-                    html newsLetterInstance.newsLetter.body
+                catch (exception) {
+                    newsLetterLog.status = 'error'
+                    newsLetterLog.errorMessage = exception.message
+                    newsLetterLog.stackTrace = exception.stackTrace
                 }
-                newsLetterLog.status = 'sent'
-            }
-            catch (exception) {
-                newsLetterLog.status = 'error'
-                newsLetterLog.errorMessage = exception.message
-                newsLetterLog.stackTrace = exception.stackTrace
-            }
-            newsLetterLog.save()
+                newsLetterLog.save()
 
-            use(TimeCategory) {
-                schedule(new Date() + 5.second, [newsLetterInstance: newsLetterInstance])
+                use(TimeCategory) {
+                    schedule(new Date() + 5.second, [newsLetterInstance: newsLetterInstance])
+                }
+            } else {
+                newsLetterInstance.finishDate = new Date()
+                newsLetterInstance.status = 'finished'
+                newsLetterInstance.save()
             }
-        } else {
-            newsLetterInstance.finishDate = new Date()
-            newsLetterInstance.status = 'finished'
-            newsLetterInstance.save()
         }
     }
 }
--- grails-app/views/layouts/goldaan/_footer.gsp
+++ grails-app/views/layouts/goldaan/_footer.gsp
@@ -1,5 +1,5 @@
 <g:if test="${actionName!='product'}">
-    <g:render template="goldaan/bigFooter"/>
+    <g:render template="/site/goldaan/bigFooter"/>
 </g:if>
 <div class="footer">
     <g:message code="copyright-goldaan"/>
