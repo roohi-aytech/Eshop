1221c5f6482d50917d31c1d17c65a7abff2a25cf
roohi <srmo642002@gmail.com>
2014-08-23 19:32:20 +0430

    93/6/1

--- grails-app/conf/GoldaanMailConfig.groovy
+++ grails-app/conf/GoldaanMailConfig.groovy
@@ -3,7 +3,7 @@ grails.mail.default.from="Goldaan.ir <noreply@goldaan.ir>"
 grails {
     mail {
         ssl = "off"
-        host = '127.0.0.1'//"192.168.52.24"
+        host = "192.168.52.24"
         from = "noreply@goldaan.ir"
         port = 25
         ssl = "off"
