8e46da378caced77b94c4404b544c47f2a83c3f4
roohi <srmo642002@gmail.com>
2014-10-21 12:15:13 +0330

    goldaan 93/07/22

---
+++ grails-app/controllers/eshop/AppController.groovy
@@ -0,0 +1,31 @@
+package eshop
+
+import grails.converters.JSON
+
+class AppController {
+
+    def mostVisited() {
+        def productType = ProductType.get(params.ptId)
+        def offset = params.int('offset') ?: 0
+        def products = Product.createCriteria().listDistinct {
+            models {
+                eq('status', 'exists')
+            }
+            or {
+                isNull('isVisible')
+                eq('isVisible', true)
+            }
+            if (productType) {
+                productTypes {
+                    'in'('id', productType?.allChildren?.collect { it.id } + [productType.id])
+                }
+            }
+            maxResults(20)
+            offset(offset)
+            order("visitCount", "desc")
+        }.collect {
+            [id: it.id, title: "${product?.manualTitle ? product?.pageTitle : "${product?.productTypes?.find { true }?.name ?: ""} ${product?.type?.title ?: ""} ${product?.brand?.name ?: ""} ${product?.name ?: ""}"}"]
+        }
+        render products as JSON
+    }
+}
