0f9cef4c02934ece487737ad50775a0bc7bf84fd
roohi <srmo642002@gmail.com>
2014-10-23 10:21:50 +0330

    goldaan 93/07/22

--- grails-app/controllers/eshop/AppController.groovy
+++ grails-app/controllers/eshop/AppController.groovy
@@ -30,7 +30,7 @@ class AppController {
                     title      : "${product?.manualTitle ? product?.pageTitle : "${product?.productTypes?.find { true }?.name ?: ""} ${product?.type?.title ?: ""} ${product?.brand?.name ?: ""} ${product?.name ?: ""}"}",
                     price      : formatNumber(number: priceService.calcProductPrice(product.id).showVal, type: 'number'),
 //                    description: product.description,
-                    modelCount : product?.models?.findAll { it.status == 'exists' }.collect {
+                    models : product?.models?.findAll { it.status == 'exists' }.collect {
                         [
                                 id:it.id,
                                 title: it.name
