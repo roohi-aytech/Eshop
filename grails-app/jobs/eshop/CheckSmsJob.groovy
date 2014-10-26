cc2e05e23c1adb6c8ddff61e9349e456bdb92f4a
roohi <srmo642002@gmail.com>
2014-10-26 14:01:09 +0330

    goldaan 93/07/22

--- grails-app/jobs/eshop/CheckSmsJob.groovy
+++ grails-app/jobs/eshop/CheckSmsJob.groovy
@@ -1,21 +1,21 @@
 package eshop
 
 
-
 class CheckSmsJob {
     def messageService
     def mailService
+    def grailsApplication
     static triggers = {
         simple repeatInterval: 86400000, startDelay: 6000 // execute job once in 2 minutes
     }
 
     def execute() {
         println 'checking remaining sms: '
-        def res=messageService.info
+        def res = grailsApplication.config.instance + " " + messageService.info
         println res
         mailService.sendMail {
             to 'info@agah-it.com'
-            subject 'remaining sms '+res
+            subject 'remaining sms ' + res
             body res
         }
     }
