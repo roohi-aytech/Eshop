842459ab4223b0f005ab46df2e813d7842834e77
roohi <srmo642002@gmail.com>
2014-11-27 10:03:39 +0330

    93/09/06

--- grails-app/controllers/eshop/AppController.groovy
+++ grails-app/controllers/eshop/AppController.groovy
@@ -208,7 +208,8 @@ class AppController {
 
             if (customerInstance.validate() && customerInstance.save()) {
                 def customerRole = Role.findByAuthority(RoleHelper.ROLE_CUSTOMER)
-                UserRole.create customerInstance, customerRole
+                if(!UserRole.countByRoleAndUser(customerRole,customerInstance))
+                    UserRole.create customerInstance, customerRole
                 def device = MobileDevice.findByDeviceCode(params.code) ?: new MobileDevice(deviceCode: params.code)
                 device.user = customerInstance
                 device.save()
