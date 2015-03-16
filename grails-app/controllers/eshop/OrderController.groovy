5f2d766e44a86112d560a9fb34b3151018d4e497
roohi <srmo642002@gmail.com>
2015-03-16 14:26:43 +0330

    goldaan.banner

--- grails-app/controllers/eshop/OrderController.groovy
+++ grails-app/controllers/eshop/OrderController.groovy
@@ -12,6 +12,13 @@ import fi.joensuu.joyds1.calendar.JalaliCalendar
 import grails.plugins.springsecurity.Secured
 import org.omg.CORBA.Environment
 
+import javax.net.ssl.HttpsURLConnection
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
+import java.security.GeneralSecurityException
+import java.security.cert.X509Certificate
+
 class OrderController {
 
     def springSecurityService
@@ -626,7 +633,29 @@ class OrderController {
         model.onlinePayment = onlinePayment
 
         double state = -100;
+
         if (status.equals("OK")) {
+            TrustManager[] trustAllCerts = new TrustManager[] {
+                new X509TrustManager() {
+                    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                        return new X509Certificate[0];
+                    }
+                    public void checkClientTrusted(
+                            java.security.cert.X509Certificate[] certs, String authType) {
+                    }
+                    public void checkServerTrusted(
+                            java.security.cert.X509Certificate[] certs, String authType) {
+                    }
+                }
+            };
+
+// Install the all-trusting trust manager
+            try {
+                SSLContext sc = SSLContext.getInstance("SSL");
+                sc.init(null, trustAllCerts, new java.security.SecureRandom());
+                HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            } catch (GeneralSecurityException e) {
+            }
             state = samanService.verifyPayment(onlinePayment.account, referenceNumber)
         }
         model.verificationResult = state.toInteger()
