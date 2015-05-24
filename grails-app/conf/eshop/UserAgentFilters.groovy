272015f9548ee0898bcb8307d78c7f133d92f1ef
roohi <srmo642002@gmail.com>
2015-05-24 08:03:38 +0430

    bank MEB

--- grails-app/conf/eshop/UserAgentFilters.groovy
+++ grails-app/conf/eshop/UserAgentFilters.groovy
@@ -7,18 +7,22 @@ class UserAgentFilters {
     def filters = {
         all(controller: '*', action: '*') {
             before = {
+                try {
+                    session["mobile"] = userAgentIdentService.isMobile()
 
-                session["mobile"] = userAgentIdentService.isMobile()
-
-                def userAgent = userAgentIdentService.userAgentString.toLowerCase()
-                if (userAgentIdentService.browser.toLowerCase().contains('robot') &&
-                        !userAgent.contains('googlebot') &&
-                        !userAgent.contains('bingbot') &&
-                        !userAgent.contains('ask jeeves/teoma') &&
-                        !userAgent.contains('yahoo')) {
-                    response.setStatus(403);
-                    response.flushBuffer()
-                    return false
+                    def userAgent = userAgentIdentService?.userAgentString?.toLowerCase()
+                    if (userAgentIdentService?.browser?.toLowerCase()?.contains('robot') &&
+                            !userAgent?.contains('googlebot') &&
+                            !userAgent?.contains('bingbot') &&
+                            !userAgent?.contains('ask jeeves/teoma') &&
+                            !userAgent?.contains('yahoo')) {
+                        response.setStatus(403);
+                        response.flushBuffer()
+                        return false
+                    }
+                }catch (x){
+                    printf "Handled Exception"
+                    x.printStackTrace()
                 }
             }
             after = { Map model ->
